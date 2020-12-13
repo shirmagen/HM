53e344d9f64bf2dbe554d7d6080dfa3625fdd409
Shirma <shirmagen095@gmail.com>
2020-12-13 12:07:27 +0200

    feat(home): add basic search, menu, add, toolbar components

--- src/Home/index.js
+++ src/Home/index.js
@@ -2,15 +2,30 @@ import React, {useState} from "react";
 import Map from '../components/Map';
 import Drawer from '../components/Drawer';
 import Tabs from '../components/Tabs';
+import SearchBar from '../components/SearchBar';
 import {Row, Column} from 'mui-flex-layout';
-import {Toolbar, AppBar, IconButton} from '@material-ui/core';
-import {Menu} from '@material-ui/icons';
+import {Toolbar, AppBar, IconButton, ButtonGroup, Button, TextField} from '@material-ui/core';
+import {Menu, Create, FlashOn, QueryBuilderOutlined, AddCircle} from '@material-ui/icons';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 import styled from 'styled-components';
 
 const ShirsHamburger = styled(IconButton)`
-  color: red;
-  width: 150px;
-  height: 150px;
+  color: grey;
+  width: 50px;
+  height: 50px;
+`;
+
+const HMVerticalToolBar = styled(ButtonGroup)`
+  position:absolute;
+  top:0;
+  left:0;
+  bottom:0;
+`;
+
+const HMCreateButton = styled(IconButton)`
+  position:absolute;
+  right:0;
+  bottom:0;
 `;
 
 export default () => {
@@ -25,16 +40,21 @@ export default () => {
   }
 
   return (
-    <Column width={'100%'} height={'100%'}>
-    <AppBar position={'static'} color={'primary'}>
-    <Toolbar variant={'dense'}>
-      <ShirsHamburger onClick={openDrawer}><Menu/></ShirsHamburger>
-      {'My Toolbar'}
-      </Toolbar>
-      </AppBar>
-      <Tabs/>
-      <Map/>
-      <Drawer onClose={closeDrawer} opened={openedDrawer} />
+    <Row width={'100%'} height={'100%'}>
+    <ShirsHamburger onClick={openDrawer}><Menu/></ShirsHamburger>
+      <Column>
+       <SearchBar/>
+        <Map/>
       </Column>
+      <Drawer onClose={closeDrawer} opened={openedDrawer} />
+      <HMVerticalToolBar
+        orientation="vertical"
+        color="primary">
+        <IconButton><Create/></IconButton>
+        <IconButton><FlashOn/></IconButton>
+        <IconButton><QueryBuilderOutlined/></IconButton>
+      </HMVerticalToolBar>
+      <HMCreateButton><IconButton><AddCircle/></IconButton></HMCreateButton>
+      </Row>
   );
 };
