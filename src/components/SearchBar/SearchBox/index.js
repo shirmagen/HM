0804dce8fb4f973c2342e0bfceb4d3e7ef2e626e
Shirma <shirmagen095@gmail.com>
2020-12-15 13:31:34 +0200

    fix(app-bar): (search-bar) separate search box from search bar

---
+++ src/components/SearchBar/SearchBox/index.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import styled from 'styled-components';
+
+const ApolloSearchBar = styled(Autocomplete)`
+  width: 90%;
+`;
+
+const getCountries = async ({active, setOptions}) => {
+    const response = await fetch('https://country.register.gov.uk/records.json?page-size=5000');
+    const countries = await response.json();
+
+    if (active) {
+      setOptions(Object.keys(countries).map((key) => countries[key].item[0]));
+    }
+  };
+
+export default () => {
+  const [open, setOpen] = useState(false);
+  const [options, setOptions] = useState([]);
+  const loading = open && options.length === 0;
+
+  useEffect(() => {
+    let active = true;
+
+    if (!loading) {
+      return undefined;
+    }
+
+    getCountries({active, setOptions});
+    return () => {
+      active = false;
+    };
+  }, [loading]);
+
+  return (
+    <ApolloSearchBar
+      groupBy={(option) => option.firstLetter}
+      open={open}
+      onOpen={() => {
+        setOpen(true);
+      }}
+      onClose={() => {
+        setOpen(false);
+      }}
+      getOptionSelected={(option, value) => option.name === value.name}
+      getOptionLabel={(option) => option.name}
+      options={options}
+      loading={loading}
+      renderInput={(params) => (
+        <TextField
+          label="חפש שירים ואלבומים"
+          {...params}
+          InputProps={{
+            ...params.InputProps,
+            endAdornment: <>{params.InputProps.endAdornment}</>,
+          }}
+        ></TextField>
+      )}
+    />
+  );
+};
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,13 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
-import styled from 'styled-components';
 import { Row } from 'mui-flex-layout';
 import { Search } from '@material-ui/icons';
-
-const HMSearchBar = styled(Autocomplete)`
-  width: 90%;
-`;
+import SearchBox from './SearchBox';
+import styled from 'styled-components';
 
 const HMSearchIcon = styled(Search)`
    padding-top: 15px;
@@ -16,60 +12,9 @@ const HMSearchIcon = styled(Search)`
 `;
 
 export default () => {
-  const [open, setOpen] = useState(false);
-  const [options, setOptions] = useState([]);
-  const loading = open && options.length === 0;
-
-  useEffect(() => {
-    let active = true;
-
-    if (!loading) {
-      return undefined;
-    }
-
-    (async () => {
-      const response = await fetch('https://country.register.gov.uk/records.json?page-size=5000');
-      const countries = await response.json();
-
-      if (active) {
-        setOptions(Object.keys(countries).map((key) => countries[key].item[0]));
-      }
-    })();
-    return () => {
-      active = false;
-    };
-  }, [loading]);
-
   return (
     <Row width={'100%'}>
-      <HMSearchBar
-        groupBy={(option) => option.firstLetter}
-        open={open}
-        onOpen={() => {
-          setOpen(true);
-        }}
-        onClose={() => {
-          setOpen(false);
-        }}
-        getOptionSelected={(option, value) => option.name === value.name}
-        getOptionLabel={(option) => option.name}
-        options={options}
-        loading={loading}
-        renderInput={(params) => (
-          <TextField
-            label="חפש שירים ואלבומים"
-            {...params}
-            InputProps={{
-              ...params.InputProps,
-              endAdornment: (
-                <>
-                  {params.InputProps.endAdornment}
-                </>
-              ),
-            }}
-          ></TextField>
-        )}
-      />
+      <SearchBox/>
         <HMSearchIcon />
     </Row>
   );
