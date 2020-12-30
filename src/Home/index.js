b34ca307b3f4e85095fdb87465226a3ed6164490
Shirma <shirmagen095@gmail.com>
2020-12-30 12:00:31 +0200

    fix(home):(app-bar)

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,23 +1,17 @@
-import React, { createContext, useState } from 'react';
+import React from 'react';
 import Map from '../components/Map';
 import VerticalToolBar from '../components/VerticalToolBar';
 import CreateButton from '../components/CreateNew';
 import ApolloAppBar from '../components/AppBar';
-import DrawToolBar from '../components/DrawToolBar';
 import { Row } from 'mui-flex-layout';
-import FlashMode from '../components/FlashMode';
-import HomeContext from './HomeContext';
 
 export default () => {
   return (
-    // <HomeContext>
     <Row width={'100%'} height={'100%'}>
-      {/* <ApolloAppBar /> */}
+      <ApolloAppBar />
       <Map />
       <VerticalToolBar />
       <CreateButton />
-      {/* <FlashMode /> */}
     </Row>
-    // </HomeContext>
   );
 };
