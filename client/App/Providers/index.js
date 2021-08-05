522304feb72a237044d86b87925342cc46c1c711
Shir Magen <shirmagen095@gmail.com>
2021-08-05 17:00:47 +0300

    feat(alert-provider): (providers) added alert provider

--- client/App/Providers/index.js
+++ client/App/Providers/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import AuthProvider from './AuthProvider';
 import ThemeProvider from './ThemeProvider';
 import LoadingProvider from './LoadingProvider';
+import AlertProvider from './AlertProvider';
 
 export default ({ children }) => (
   <ThemeProvider>
     <LoadingProvider>
-      <AuthProvider>{children}</AuthProvider>
+      <AlertProvider>
+        <AuthProvider>{children}</AuthProvider>
+      </AlertProvider>
     </LoadingProvider>
   </ThemeProvider>
 );
