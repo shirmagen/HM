235e44d117f9b53e291265f3822d168a16ef755b
omerdital <omerdital@gmail.com>
2021-08-26 12:28:39 +0300

    wip

--- client/App/Shell/Home/index.tsx
+++ client/App/Shell/Home/index.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import {Map} from '../../components/Map';
-import {VerticalToolBar} from '../../components/VerticalToolBar';
-import {CreateButton} from '../../components/CreateNew/CreateNewButton';
-import {AppBar} from '../../components/AppBar';
+import {NavBar} from '../../components/AppBar';
 import { Row } from 'mui-flex-layout';
 
 export const Home = () =>
   (
     <Row width={'100%'} height={'100%'}>
-      <AppBar/>
-      <Map/>
-      <VerticalToolBar/>
-      <CreateButton/>
-    </Row>
+      <NavBar/>
+     </Row>
   );
--- client/App/components/AppBar/index.tsx
+++ client/App/components/AppBar/index.tsx
@@ -1,43 +1,18 @@
 import React, { useState } from 'react';
-import { Row } from 'mui-flex-layout';
-import { IconButton } from '@material-ui/core';
-import { Menu } from '@material-ui/icons';
-import {SearchBar} from '../SearchBar/SearchBar';
-import {ApolloDrawer} from '../Drawer';
+import { AppBar, Grid, Toolbar } from '@material-ui/core';
 import styled from 'styled-components';
 
-const ApolloMenu = styled(IconButton)`
-  width: 50px;
-  height: 50px;
+const ApolloToolbar = styled(Toolbar)`
+background-color: white;
+color: black;
 `;
 
-const ApolloAppBar = styled(Row)`
-color: grey;
-position: absolute;
-width: 250px;
-height: 50px;
-background: white;
-z-index: 1;
-`;
-
-export const AppBar = () => {
-  const [openedDrawer, setOpenedDrawer] = useState(false);
-
-  const openDrawer = () => {
-    setOpenedDrawer(true);
-  };
-
-  const closeDrawer = () => {
-    setOpenedDrawer(false);
-  };
-
+export const NavBar = () => {
   return (
-    <ApolloAppBar>
-      <ApolloMenu onClick={openDrawer}>
-        <Menu />
-      </ApolloMenu>
-      <SearchBar />
-      <ApolloDrawer onClose={closeDrawer} opened={openedDrawer} />
-    </ApolloAppBar>
-  );
+    <AppBar>
+      <ApolloToolbar>
+        צהריים טובים נגה
+      </ApolloToolbar>
+    </AppBar>
+  )
 };
